fda2b23da6b4e31fdcd28ee1c017563cd0b27952
Meyer <meyera36@emea.roche.com>
2020-08-21 14:26:55 +0200

    Add project files.

---
+++ M403 Programm-Header/M403 Programm-Header.cpp
@@ -0,0 +1,59 @@
+/*******************************************************************************
+* Programm:   Projekt-Header
+* Filename:   M403 Projekt Header.cpp
+*
+* Autor:      Alina Meyer
+* Version:    1.0
+* Datum:      21.08.2020
+*
+* Entwicklungsablauf(Version, Datum, Autor, Entwicklungsschritt, Zeit):
+* 1.0
+********************************************************************************
+*
+* Verwendungszweck: C-Schulung
+*
+* Beschreibung:     Header erstellen als Vorlage für zukünftige Projekte
+*
+*
+* Precondition:  -
+*
+* Postcondition: -
+*
+* Benötigte Libraries:
+* - iostream
+*
+* Copyright (c) 2020 by A.Meyer, CH-6300 Zug
+*******************************************************************************/
+
+/*** Include Files ***********************************************************/
+#include <iostream>
+
+/***  Globale Deklarationen und Definitionen **********************************/
+
+
+/***  Funktions-Deklarationen *************************************************/
+
+
+
+/*******************************************************************************
+******************************* HAUPTPROGRAMM **********************************
+*******************************************************************************/
+int main(void) {
+
+    // lokale Variablen                                                                  
+
+
+    /* Intro --------------------- */
+
+
+    /* Eingabe ------------------- */
+
+
+    /* Verarbeitung -------------- */
+
+
+    /* Ausgabe ------------------- */
+
+
+    return (0);
+}
